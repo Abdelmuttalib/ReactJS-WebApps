86282c3ac6f0c70965526b152d5fd6e3e6733afc
Your Name <hagar9886@gmail.com>
2021-02-07 15:28:58 +0800

    update index.js

---
+++ tic-tao-toe/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+const index = () => {
+    return (
+        <div>
+            HEllo
+        </div>
+    )
+}
